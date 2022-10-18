a33c904ccd8be9e09fdab25964eb76d198fe000c
khivus <aplls5643@yandex.ru>
2022-10-19 00:50:53 +0300

    Smol update

--- tgbot.cpp
+++ tgbot.cpp
@@ -2,19 +2,21 @@
 //
 // for compilation and start:
 // g++ tgbot.cpp -o telegram_bot --std=c++14 -I/usr/local/include -lTgBot -lboost_system -lssl -lcrypto -lpthread -lcurl && ./telegram_bot
-//
+// 
 
 #include "token.h" // including private token
-
-#include "sync-request.json"
-#include "srproject-364912-1f4f2c308940.json"
+// #include "google/cloud/speech/speech_client.h"
+// #include "google/cloud/project.h"
 
 #include <stdio.h>
 #include <tgbot/tgbot.h>
 #include <string>
 #include <iostream>
 #include <curl/curl.h>
+#include <fstream>
+#include <nlohmann/json.hpp>
 
+using json = nlohmann::json;
 using namespace std;
 
 static size_t write_data(void *ptr, size_t size, size_t nmemb, void *stream) { // Func for download file with curl
@@ -22,7 +24,43 @@ static size_t write_data(void *ptr, size_t size, size_t nmemb, void *stream) { /
   return written;
 }
 
-void get_response(string url) { // Func for get voice file
+void update_json(string uri, string lang) {
+    std::ifstream input_file("sync-request.json");
+    json data = json::parse(input_file);
+    data["config"]["languageCode"] = lang;
+    data["audio"]["uri"] = uri;
+    input_file.close();
+
+    std::ofstream output_file;
+    output_file.open("sync-request.json", std::fstream::out | std::fstream::trunc);
+    output_file << data.dump() << std::endl;
+    output_file.close();
+}
+
+void send_voice(string url) {
+    CURL *curl;
+    CURLcode res;
+
+    struct curl_httppost *post = NULL;
+    struct curl_httppost *last = NULL;
+
+    curl_global_init(CURL_GLOBAL_ALL);
+    curl = curl_easy_init();
+
+    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+
+    curl_formadd(
+        &post, &last,
+        CURLFORM_FILE, "sync-request.json",
+        CURLFORM_END
+    );
+
+    curl_easy_cleanup(curl);
+    curl_global_cleanup();
+}
+
+void get_voice(string url) { // Func for get voice file
     CURL *curl;
     CURLcode res;
     FILE *audiofile;
@@ -44,11 +82,12 @@ void get_response(string url) { // Func for get voice file
 
 int main() {
     TgBot::Bot bot(token);
+
     bot.getEvents().onCommand("start", [&bot](TgBot::Message::Ptr message) { // If command /start
         bot.getApi().sendMessage(message->chat->id, "Hi!");
     });
 
-    bot.getEvents().onCommand("getreply", [&bot](TgBot::Message::Ptr message) { // If command /getreply. This is test name
+    bot.getEvents().onCommand("convert", [&bot](TgBot::Message::Ptr message) { // If command /getreply. This is test name
         if (message->replyToMessage != NULL && message->replyToMessage->voice != NULL) {
             printf("Bot got replied voice message.\n" // Output to console voice msg data
                     "Voice data is: [ %s, %s, %i, %s, %li ]\n", 
@@ -60,19 +99,31 @@ int main() {
             auto file_path = bot.getApi().getFile(message->replyToMessage->voice->fileId); // Getting file path
             string file_url = "https://api.telegram.org/file/bot" + token + "/" + file_path->filePath; // Generating link to the voice msg file
             cout << file_url << "\n"; // Output the link
-            get_response(file_url); // Downloading file
+            get_voice(file_url); // Downloading file
             string text; // There are will be Speech-to-Text recognition
-            bot.getApi().sendMessage(message->chat->id, "VTT", false, message->replyToMessage->messageId); // Outputting recognized text. "VTT" is a placeholder
+            bot.getApi().sendMessage(message->chat->id, "Placeholder", false, message->replyToMessage->messageId); // Outputting recognized text.
         }
         else {
             bot.getApi().sendMessage(message->chat->id, "Reply to the voice message and use this command again."); // If no replyed message and message isn't a voice message
         }
     });
 
+    bot.getEvents().onCommand("language", [&bot](TgBot::Message::Ptr message) { // If command /start
+        string lang = "en-US";
+        string msg;
+        if (lang == "ru-Ru") {
+            msg = "Выберите предпочетаемый язык.";
+        }
+        else {
+            msg = "Select preferred language.";
+        }
+        bot.getApi().sendMessage(message->chat->id, msg);
+    });
+
     bot.getEvents().onAnyMessage([&bot](TgBot::Message::Ptr message) {
         printf("User wrote: %s\n", message->text.c_str());
-
     });
+
     try {
         printf("Logged on with username: %s\n", bot.getApi().getMe()->username.c_str());
         TgBot::TgLongPoll longPoll(bot);
@@ -83,5 +134,6 @@ int main() {
     } catch (TgBot::TgException& e) {
         printf("error: %s\n", e.what());
     }
+    
     return 0;
 }
